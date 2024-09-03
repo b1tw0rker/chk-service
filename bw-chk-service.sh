8d2023d8b83529856f2e35fce362038c5365f0e0
b1w0rker <mail@bit-worker.com>
2024-09-03 08:57:57 +0200

    initial commit

---
+++ bw-chk-service.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### vars
+###
+###
+MSG_ACHTUNG="\e[31m[ACHTUNG]\e[0m"
+MSG_OK="\e[32m[OK]\e[0m"
+
+### load library
+###
+###
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/bw-chk-service-lib.sh"
+
+### Start
+###
+###
+case "$1" in
+dev)
+    STDIN="dev"
+    chk_services
+    #chk_kernel_panic
+    echo ""
+    ;;
+prod)
+    chk_services
+    ;;
+ex)
+    STDIN="dev"
+    chk_local_services
+    ;;
+*)
+    echo "Ungültiges Argument. Bitte 'dev' , 'prod' oder 'ex' verwenden."
+    exit 1
+    ;;
+esac
+
